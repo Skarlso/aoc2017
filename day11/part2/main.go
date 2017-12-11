645968422ce2816c5ea3ec984b3a45cfae9a9830
Gergely Brautigam <skarlso777@gmail.com>
2017-12-11 22:21:01 +0100

    Part 2 works.

--- day11/part2/main.go
+++ day11/part2/main.go
@@ -8,16 +8,24 @@ import (
 	"strings"
 )
 
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+
+	return x
+}
+
 func main() {
 	content, err := ioutil.ReadFile("../input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	x := 0
-	y := -1
-	z := 1
-	steps := strings.Split(string(content), ",")
+	y := 0
+	z := 0
 	max := 0
+	steps := strings.Split(strings.TrimSpace(string(content)), ",")
 	for _, step := range steps {
 		switch step {
 		case "n":
@@ -43,6 +51,7 @@ func main() {
 		max = int(math.Max(float64(max), math.Abs(float64(y))))
 		max = int(math.Max(float64(max), math.Abs(float64(z))))
 	}
-	fmt.Println(x, y, z)
-	fmt.Println(max + 1)
+	distance := (abs(x) + abs(y) + abs(z)) / 2
+	fmt.Println(distance)
+	fmt.Println(max)
 }
