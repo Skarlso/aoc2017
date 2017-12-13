57e7e3211eac2e2c922afc2ef5236446eaa9494f
Gergely Brautigam <skarlso777@gmail.com>
2017-12-13 06:53:37 +0100

    Started on day12.

---
+++ day12/part1/app.py
@@ -0,0 +1,17 @@
+"""
+day 12 Part 1
+"""
+# pylint: disable-msg=C0103
+import re
+
+pipes = {}
+
+with open("../sample.txt", "r") as f:
+    data = f.readlines()
+
+for line in data:
+    m = re.match(r"(?P<pipe>\d+)\s<->\s(?P<connections>[\d+,\s]+)", line)
+    pipe = m.group("pipe")
+    connections = m.group("connections")
+    print(pipe)
+    print(connections.strip().split(','))
