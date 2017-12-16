850b07239cb59884f0478dee422d067948cd21ce
Gergely Brautigam <skarlso777@gmail.com>
2017-12-16 14:50:15 +0100

    Day 15 Part 1 & 2 DONE.

--- day15/part2/main.go
+++ day15/part2/main.go
@@ -5,17 +5,23 @@ import "fmt"
 type Generator struct {
 	curr   int64
 	factor int64
+	mult   int64
 }
 
 func (g *Generator) generateNew() {
-	g.curr = (g.curr * g.factor) % 2147483647
+	for {
+		g.curr = (g.curr * g.factor) % 2147483647
+		if g.curr%g.mult == 0 {
+			break
+		}
+	}
 }
 
 func main() {
-	genA := Generator{curr: 722, factor: 16807}
-	genB := Generator{curr: 354, factor: 48271}
+	genA := Generator{curr: 722, factor: 16807, mult: 4}
+	genB := Generator{curr: 354, factor: 48271, mult: 8}
 	found := 0
-	for i := 0; i < 40000000; i++ {
+	for i := 0; i < 5000000; i++ {
 		genA.generateNew()
 		genB.generateNew()
 		if (genA.curr & 0xffff) == (genB.curr & 0xffff) {
