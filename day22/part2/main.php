f328fe656a7bb7d581340cf83e0b9f630a4aba87
Gergely Brautigam <skarlso777@gmail.com>
2017-12-22 22:28:49 +0100

    Day 22 DOnneeeEE

---
+++ day22/part2/main.php
@@ -0,0 +1,153 @@
+<?php
+
+class Direction {
+    private $left;
+    private $right;
+    private $name;
+    private $reversed;
+
+    public function __construct($y, $x, $name)
+    {
+        $this->x = $x;
+        $this->y = $y;
+        $this->name = $name;
+    }
+
+    public function setRight(Direction $direction)
+    {
+        $this->right = $direction;
+    }
+
+    public function setLeft(Direction $direction)
+    {
+        $this->left = $direction;
+    }
+
+    public function getRight(): Direction
+    {
+        return $this->right;
+    }
+
+    public function getLeft(): Direction
+    {
+        return $this->left;
+    }
+
+    public function getX(): int
+    {
+        return $this->x;
+    }
+
+    public function getY(): int
+    {
+        return $this->y;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setReversed(Direction $direction)
+    {
+        $this->reversed = $direction;
+    }
+
+    public function getReversed(): Direction
+    {
+        return $this->reversed;
+    }
+}
+
+$UP = new Direction(-1, 0, 'up');
+$DOWN = new Direction(1, 0, 'down');
+$LEFT = new Direction(0, -1, 'left');
+$RIGHT = new Direction(0, 1, 'right');
+$UP->setLeft($LEFT);
+$UP->setRight($RIGHT);
+$UP->setReversed($DOWN);
+
+$DOWN->setLeft($RIGHT);
+$DOWN->setRight($LEFT);
+$DOWN->setReversed($UP);
+
+$LEFT->setLeft($DOWN);
+$LEFT->setRight($UP);
+$LEFT->setReversed($RIGHT);
+
+$RIGHT->setLeft($UP);
+$RIGHT->setRight($DOWN);
+$RIGHT->setReversed($LEFT);
+
+// $BURSTS = 10000;
+
+$filename = $argv[1];
+$content = file_get_contents($filename, 'r');
+$infectionCount = 0;
+$BURSTS = intval($argv[2]);
+
+foreach (explode("\n", $content) as $key => $value) {
+    $grid[] = str_split($value);
+}
+
+$startingX = intval(sizeof($grid[0])/2);
+$startingY = intval(sizeof($grid[0])/2);
+
+echo "StartingX: $startingX; StartingY: $startingY\n";
+$currentDirection = $UP;
+
+function display() {
+    global $grid;
+    foreach ($grid as $key => $value) {
+        echo implode("", $value) . "\n";
+    }
+}
+
+function move(Direction $direction, int $x, int $y) {
+    global $grid;
+    $x += $direction->getX();
+    $y += $direction->getY();
+    if (!array_key_exists($y, $grid)) {
+        $grid[$y] = ["."];
+    }
+    if (empty($grid[$y][$x])) {
+        $grid[$y][$x] = ".";
+    }
+    return [$x, $y];
+}
+
+function turn(Direction $direction, int $x, int $y): Direction {
+    global $grid;
+    if ($grid[$y][$x] === "#") {
+        $direction = $direction->getRight();
+    } else if ($grid[$y][$x] === ".") {
+        $direction = $direction->getLeft();
+    } else if ($grid[$y][$x] === "F") {
+        $direction = $direction->getReversed();
+    }
+    return $direction;
+}
+
+function infect(int $x, int $y) {
+    global $grid, $infectionCount;
+    $char = $grid[$y][$x];
+    if ($char === '.') {
+        $grid[$y][$x] = 'W';
+    } else if ($char === 'W') {
+        $grid[$y][$x] = '#';
+        $infectionCount++;
+    } else if ($char === '#') {
+        $grid[$y][$x] = 'F';
+    } else if ($char === 'F') {
+        $grid[$y][$x] = '.';
+    }
+}
+
+$x = $startingX;
+$y = $startingY;
+for ($i=0; $i < $BURSTS; $i++) {
+    $currentDirection = turn($currentDirection, $x, $y);
+    infect($x, $y);
+    list($x, $y) = move($currentDirection, $x, $y);
+}
+print("Infection count: $infectionCount.");
