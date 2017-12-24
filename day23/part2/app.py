16ff33db09263e00ad2377482b0228556ada831c
Gergely Brautigam <skarlso777@gmail.com>
2017-12-24 21:02:05 +0100

    Added explanation why this is the solution for part2.

--- day23/part2/app.py
+++ day23/part2/app.py
@@ -1,5 +1,12 @@
 """
 day 23 Part 2
+
+For future me: Why is this the solution?
+At first, I re-wrote that sample into a Python program and run that. It run for a little while,
+but spat out 909. I tried and it did work. Than I thought, that it has to be faster than that,
+so I kept on checking what it was actually doing, now that I re-wrote it in Python.
+After I saw the code better, I saw that it actually is counting non prime numbers.
+And thus, this is the correct solution.
 """
 from math import sqrt, floor
 
